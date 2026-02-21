bb3d3c3e34ecc4665a35ea4f2baa55352161f438
Alimur Razi Rana <alimurrazi570@gmail.com>
2026-02-21 19:55:42 +0600

    feat: dependecy added

--- ProductService.Infrastructure/Persistence/ProductDbContext.cs
+++ ProductService.Infrastructure/Persistence/ProductDbContext.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ProductService.Infrastructure.Persistence
 {
-    internal class ProductDbContext
+    public class ProductDbContext: DbContext
     {
+
     }
 }
